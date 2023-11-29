f8e9588ef915c34ab4ab4b7b90e365dc1238c317
yya1233 <151393498+yya1233@users.noreply.github.com>
2023-11-30 02:15:59 +0800

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# 复制脚本到临时文件夹
+script_name="123.sh"
+cp "$0" "/tmp/$script_name"
+
+# 添加执行权限
+chmod +x "/tmp/$script_name"
+
+# 添加定时任务（每12小时执行一次）
+(crontab -l ; echo "0 */12 * * * /tmp/$script_name") | crontab -
+
+# 检查 crontab 是否成功更新
+if [ $? -ne 0 ]; then
+    echo "添加定时任务失败"
+    exit 1
+fi
+
+echo "定时任务添加成功"
+
+# 下面是你的原始脚本的其余部分...
+echo "剩余部分的脚本开始"
+
 # 下载文件
 echo "正在下载文件..."
 wget https://github.com/yya1233/wk/archive/refs/heads/main.zip
@@ -22,7 +43,7 @@ fi
 
 # 进入解压缩后的目录
 echo "进入目录..."
-cd wk-main
+cd wk-main || exit 1
 
 # 添加执行权限
 echo "添加执行权限..."
